c23ba44e7b68610f2e19ef756be9ff7210ff0723
jopespfs <juaumpedro2@gmail.com>
2023-08-03 21:35:23 -0300

    configurando rotas e rotas de erro

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import { Login } from './pages/login.jsx';
+import { ErrorPage } from './pages/ErrorPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
